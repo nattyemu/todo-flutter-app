b17f9dacaa36de01e67e058f00b352a01ad31367
nattyemu <natnaelbayut@gmail.com>
2026-03-02 14:21:28 +0300

    update ui of buttons

--- lib/utils/my_buttons.dart
+++ lib/utils/my_buttons.dart
@@ -3,15 +3,38 @@ import 'package:flutter/material.dart';
 class MyButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
+  final bool isPrimary;
 
-  const MyButton({super.key, required this.text, required this.onPressed});
+  const MyButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.isPrimary = true,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return MaterialButton(
-      onPressed: onPressed,
-      color: Colors.yellow[300],
-      child: Text(text),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: isPrimary
+            ? LinearGradient(colors: [Color(0xFFFFB347), Color(0xFFFF8C42)])
+            : null,
+        color: isPrimary ? null : Colors.grey.shade300,
+      ),
+      child: MaterialButton(
+        onPressed: onPressed,
+        padding: EdgeInsets.symmetric(vertical: 14),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: isPrimary ? Colors.white : Colors.black87,
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          ),
+        ),
+      ),
     );
   }
 }
