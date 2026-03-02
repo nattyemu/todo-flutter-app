c9c0e8274bbe9031d1b093c62cf8ee4ab70400ad
nattyemu <natnaelbayut@gmail.com>
2026-03-02 14:22:27 +0300

    add edit functionality by passing parametrs to dialog box and update ui in todo list tile

--- lib/utils/todo_title.dart
+++ lib/utils/todo_title.dart
@@ -6,51 +6,73 @@ class ToDOTile extends StatelessWidget {
   final bool taskComplted;
   final Function(bool?)? onChanged;
   final void Function(BuildContext)? deleteTask;
+  final VoidCallback? onEdit;
+
   const ToDOTile({
     super.key,
     required this.taskName,
     required this.taskComplted,
     required this.onChanged,
     required this.deleteTask,
+    this.onEdit,
   });
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 12, right: 12, top: 12),
+      padding: const EdgeInsets.only(bottom: 12),
       child: Slidable(
         endActionPane: ActionPane(
           motion: StretchMotion(),
           children: [
             SlidableAction(
               onPressed: deleteTask,
-              backgroundColor: Colors.red,
-              icon: Icons.delete,
+              backgroundColor: Colors.red.shade400,
+              foregroundColor: Colors.white,
+              icon: Icons.delete_outline,
+              label: 'Delete',
+              borderRadius: BorderRadius.circular(15),
             ),
           ],
         ),
         child: Container(
-          padding: EdgeInsets.all(23),
           decoration: BoxDecoration(
-            color: Colors.yellow,
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Row(
-            children: [
-              Checkbox(
-                value: taskComplted,
-                onChanged: onChanged,
-                activeColor: Colors.black,
-              ),
-              Text(
-                taskName,
-                style: TextStyle(
-                  decoration: taskComplted
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none,
-                ),
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withAlpha(10),
+                blurRadius: 10,
+                offset: Offset(0, 5),
               ),
             ],
+          ),
+          child: ListTile(
+            contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            leading: Checkbox(
+              value: taskComplted,
+              onChanged: onChanged,
+              activeColor: Color(0xFFFF8C42),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(6),
+              ),
+            ),
+            title: Text(
+              taskName,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+                decoration: taskComplted
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+                color: taskComplted ? Colors.grey : Colors.black87,
+              ),
+            ),
+            trailing: IconButton(
+              onPressed: onEdit,
+              icon: Icon(Icons.edit_outlined, color: Color(0xFFFF8C42)),
+              splashRadius: 20,
+            ),
           ),
         ),
       ),
