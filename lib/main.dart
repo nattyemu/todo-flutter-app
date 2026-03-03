f683b803685cebb343a81628cf44f5b01e5b13d6
nattyemu <natnaelbayut@gmail.com>
2026-03-03 22:35:11 +0300

    mount HomeToDoPage to main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todo_app/pages/home_to_do_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Text('todo app'),
+      home: HomeToDoPage(),
       theme: ThemeData(primarySwatch: Colors.yellow),
     );
   }
